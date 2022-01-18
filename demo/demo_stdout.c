94ec0dc9dd754e4374e122dbe513b806c837d807
sunzhenliang <hisunzhenliang@outlook.com>
2022-01-18 19:31:42 +0800

    Add level change in stdout demo

--- demo/demo_stdout.c
+++ demo/demo_stdout.c
@@ -11,5 +11,15 @@ int demo_stdout_main(void) {
     log_info("this is a log.");
     log_debug("this is a log.");
 
+    log_stdout_verbose(LOG_WARN);
+    log_emergency("this is a log.");
+    log_alert("this is a log.");
+    log_critical("this is a log.");
+    log_error("this is a log.");
+    log_warn("this is a log.");
+
+    log_notice("this is a log.");
+    log_info("this is a log.");
+    log_debug("this is a log.");
     return 0;
 }
