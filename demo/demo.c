4b0b1fef4e139f92d9ca3634bc2ca220669477da
sunzhenliang <hisunzhenliang@outlook.com>
2022-01-18 18:09:42 +0800

    Fix segment fault

--- demo/demo.c
+++ demo/demo.c
@@ -14,27 +14,26 @@ static demo g_demo[] = {
 
 int main(int argc, char *argv[]) {
     log_stdout_verbose(LOG_INFO);
+    char *target_demo = argc == 2 ? argv[1] : "";
     int r = 0;
     char *name = NULL;
     size_t size = sizeof(g_demo) / sizeof(g_demo[0]);
     for (size_t i = 0; i < size; i++) {
-        if (g_demo[i].name && !strcmp(g_demo[i].name, argv[1])) {
+        if (g_demo[i].name && !strcmp(g_demo[i].name, target_demo)) {
             name = g_demo[i].name;
             r = g_demo[i].main();
             if (r) {
                 log_error("%s demo failed.", g_demo[i].name);
-            } else {
-                log_info("%s demo done.", g_demo[i].name);
             }
             break;
         }
     }
 
     if (!name) {
-        log_info("===========================================================");
-        log_info("Usages: demo [demoname]");
+        printf("===========================================================\n"
+                 "  Usages: demo [demoname]\n");
 
-        for (int i = 0; i < size; i++) log_info("demoname: %s", g_demo[i].name);
+        for (int i = 0; i < size; i++) printf("Demoname: %s\n", g_demo[i].name);
     }
 
     return r;
